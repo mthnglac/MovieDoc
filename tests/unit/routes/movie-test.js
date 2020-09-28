16f9f5210097733f9b2e4d2a8251f3f3b85efc72
Metehan Gulac <metehanglc@protonmail.com>
2020-09-29 02:08:23 +0300

    model test completed. it creating sample model and checking adult mechanism.

--- tests/unit/routes/movie-test.js
+++ tests/unit/routes/movie-test.js
@@ -5,7 +5,7 @@ module("Unit | Route | movie", function (hooks) {
   setupTest(hooks);
 
   test("it exists", function (assert) {
-    let route = this.owner.lookup("route:movies");
+    let route = this.owner.lookup("route:movie");
     assert.ok(route);
   });
 });
